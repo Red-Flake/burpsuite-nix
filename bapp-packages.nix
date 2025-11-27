92e5e2be96793e43134b925cb577f5c3130e57f1
Letgamer <alexstephan005@gmail.com>
2025-11-27 21:47:31 +0100

    simplified the package creation script

--- bapp-packages.nix
+++ bapp-packages.nix
@@ -5,53 +5,49 @@
 }:
 
 let
-  universe = lib.importTOML ./burp-extensions.toml;
+  extensions = lib.importTOML ./burp-extensions.toml;
 in
-lib.foldlAttrs (
-  acc: pname: versionSet:
+lib.mapAttrs (
+  pname: versionSet:
   let
     latest = lib.last (lib.attrNames versionSet);
     pkg = versionSet.${latest};
-
-    drv = pkgs.stdenvNoCC.mkDerivation {
-      pname = pname;
-      version = latest;
-
-      src = fetchzip {
-        inherit (pkg) hash;
-        # Workaround for: https://github.com/NixOS/nixpkgs/issues/60157
-        url = "https://portswigger.net/bappstore/bapps/download/${pkg.uuid}/${pkg.serialversion}#.zip";
-        stripRoot = false;
-      };
-
-      name = "burpsuite-extension-${pname}-${latest}";
-
-      installPhase = ''
-        runHook preInstall
-
-        mkdir -p $out/lib/${pname}
-        cp -r $src/* $out/lib/${pname}/
-
-        runHook postInstall
-      '';
-
-      meta = {
-        description = "${pkg.name} Extension for BurpSuite";
-        maintainers = with lib.maintainers; [ letgamer ];
-        license = lib.licenses.unfree;
-        homepage = "https://github.com/portswigger/${pname}";
-      };
-
-      passthru = {
-        burp = {
-          uuid = pkg.uuid;
-          serialversion = pkg.serialversion;
-          name = pkg.name;
-          extensiontype = pkg.extensiontype;
-          manifest = "${drv}/lib/${pname}/BappManifest.bmf";
-        };
-      };
-    };
   in
-  acc // { ${pname} = drv; }
-) { } universe
+  pkgs.stdenvNoCC.mkDerivation (drv: {
+    pname = pname;
+    version = latest;
+
+    src = fetchzip {
+      inherit (pkg) hash;
+      # Workaround for: https://github.com/NixOS/nixpkgs/issues/60157, append the URL Fragment to recognize it as a ZIP
+      url = "https://portswigger.net/bappstore/bapps/download/${pkg.uuid}/${pkg.serialversion}#.zip";
+      stripRoot = false;
+    };
+
+    name = "burpsuite-extension-${pname}-${latest}";
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/lib/${pname}
+      cp -r $src/* $out/lib/${pname}/
+
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "${pkg.name} Extension for BurpSuite";
+      maintainers = with lib.maintainers; [ letgamer ];
+      license = lib.licenses.unfree;
+      homepage = "https://github.com/portswigger/${pname}";
+    };
+
+    passthru.burp = {
+      uuid = pkg.uuid;
+      serialversion = pkg.serialversion;
+      name = pkg.name;
+      extensiontype = pkg.extensiontype;
+      manifest = "${drv}/lib/${pname}/BappManifest.bmf";
+    };
+  })
+) extensions
